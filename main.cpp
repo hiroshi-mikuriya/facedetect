7148072f66caccd102cb396463732f1a9f2f4e51
mikuriya <mickly9@hotmail.com>
2018-01-05 14:37:15 +0900

    refactor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <opencv2/opencv.hpp>
 #include "mycamera.hpp"
 
+namespace
+{
+    cv::Rect operator*(cv::Rect const & rc, double r)
+    {
+        return cv::Rect(static_cast<int>(r * rc.x), static_cast<int>(r * rc.y), static_cast<int>(r * rc.width), static_cast<int>(r * rc.height));
+    }
+    cv::Rect operator/(cv::Rect const & rc, double r)
+    {
+        return rc * (1 / r);
+    }
+    cv::Size operator*(cv::Size const & sz, double r)
+    {
+        return cv::Size(static_cast<int>(r * sz.width), static_cast<int>(r * sz.height));
+    }
+}
+
 int main()
 {
     MyCamera camera(0);
@@ -19,16 +35,9 @@ int main()
         std::vector<cv::Rect> rects;
         cv::Mat rimg;
         cv::resize(img, rimg, cv::Size(), r, r);
-        cv::Size min(static_cast<int>(100 * r), static_cast<int>(100 * r));
-        cascade.detectMultiScale(rimg, rects, 1.1, 5, 0, min);
+        cascade.detectMultiScale(rimg, rects, 1.1, 5, 0, cv::Size(100, 100) * r);
         for(auto rc : rects){
-            cv::Rect rc2 = { 
-                static_cast<int>(rc.x / r), 
-                static_cast<int>(rc.y / r), 
-                static_cast<int>(rc.width / r), 
-                static_cast<int>(rc.height / r)
-            };
-            cv::rectangle(img, rc2, cv::Scalar(0, 255, 0));
+            cv::rectangle(img, rc / r, cv::Scalar(0, 255, 0));
         }
         cv::imshow("image", img);
         cv::waitKey(1);

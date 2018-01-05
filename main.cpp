432b189a2f4001b7e598ca55be0452749b72dcb4
mikuriya <mickly9@hotmail.com>
2018-01-05 11:39:39 +0900

    use camera

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 
 int main()
 {
-    std::cout << "hello world." << std::endl;
+    cv::VideoCapture cap(0);
+    if(!cap.isOpened()){
+        std::cerr << "failed to open camera" << std::endl;
+    }
+    for(;;){
+        cv::Mat img;
+        cap >> img;
+        cv::imshow("image", img);
+        cv::waitKey(1);
+    }
     return 0;
 }

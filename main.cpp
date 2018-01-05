12191e4ae7580adc95cfa64f18448479d493ee52
mikuriya <mickly9@hotmail.com>
2018-01-05 12:04:52 +0900

    use mycamera

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <opencv2/opencv.hpp>
-#include <iostream>
+#include "mycamera.hpp"
 
 int main()
 {
-    cv::VideoCapture cap(0);
+    MyCamera cap(0);
     if(!cap.isOpened()){
         std::cerr << "failed to open camera" << std::endl;
     }
@@ -12,10 +12,9 @@ int main()
         cv::Mat img;
         cap >> img;
         std::vector<cv::Rect> rects;
-        cascade.detectMultiScale(img, rects, 1.1, 3, 0, { 100, 100 });
+        cascade.detectMultiScale(img, rects, 1.1, 10, 0, { 100, 100 });
         for(auto rc : rects){
             cv::rectangle(img, rc, cv::Scalar(0, 255, 0));
-            std::cout << rc << std::endl;
         }
         cv::imshow("image", img);
         cv::waitKey(1);

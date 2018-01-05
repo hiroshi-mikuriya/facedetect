ec80aca5e5fa43211cdf5a7f81386595f67240b4
mikuriya <mickly9@hotmail.com>
2018-01-05 11:56:28 +0900

    success detect face

--- main.cpp
+++ main.cpp
@@ -7,9 +7,17 @@ int main()
     if(!cap.isOpened()){
         std::cerr << "failed to open camera" << std::endl;
     }
+    std::string cascade_path = "./haarcascade_frontalface_default.xml";
     for(;;){
         cv::Mat img;
         cap >> img;
+        cv::CascadeClassifier cascade(cascade_path);
+        std::vector<cv::Rect> rects;
+        cascade.detectMultiScale(img, rects, 1.1, 3, 0, { 100, 100 });
+        for(auto rc : rects){
+            cv::rectangle(img, rc, cv::Scalar(0, 255, 0));
+            std::cout << rc << std::endl;
+        }
         cv::imshow("image", img);
         cv::waitKey(1);
     }

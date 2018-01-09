1752563b55081e6744f7d90ca55acc533ee19913
hiroshi <hiroshi@iMacHiroshi2.local>
2018-01-09 23:48:37 +0900

    add gamma

--- main.cpp
+++ main.cpp
@@ -15,6 +15,16 @@ namespace
     {
         return cv::Size(static_cast<int>(r * sz.width), static_cast<int>(r * sz.height));
     }
+    cv::Mat gamma(cv::Mat const & src, double g)
+    {
+        uchar lut[256] = { 0 };
+        for(int i = 0; i < sizeof(lut); ++i){
+            lut[i] = static_cast<int>(std::pow(i / 255.0, 1 / g) * 255);
+        }
+        cv::Mat dst;
+        cv::LUT(src, cv::Mat(cv::Size(256, 1), CV_8U, lut), dst);
+        return dst;
+    }
 }
 
 int main()
@@ -31,6 +41,7 @@ int main()
             std::cerr << "failed to get camera image." << std::endl;
             continue;
         }
+        img = gamma(img, 2.0);
         double r = 0.25;
         std::vector<cv::Rect> rects;
         cv::Mat rimg;
